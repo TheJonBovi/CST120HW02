64c05939696dd782a1e5a6ca6f29f8673c894809
Drew <35358211+TheJonBovi@users.noreply.github.com>
2018-04-15 18:16:59 -0700

    assignment completed
    added function for inputting numbers.
    added function for calculating number of digits of an int.
    IO to meet spec
    description added to top.

--- CST120HW02/main.c
+++ CST120HW02/main.c
@@ -1,18 +1,51 @@
+// main.c 
+//
+// This program takes two numbers from the console and displays the sum 
+// and product of them, as well as the number of digits in the product
+//
+
 #define _CRT_SECURE_NO_WARNINGS
 
-#include <stdio.h>
+#include <stdio.h>  // NOLINT
+
+int get_number()
+{
+	int result;
+
+	printf("Enter a positive number:\n");
+	scanf(" %d", &result);
+
+	while (result < 0)
+	{
+		printf("Entered number is not positive, please reenter a positive number:\n");
+		scanf(" %d", &result);
+	}
+
+	return result;
+}
+
+int num_digits(int const number)
+{
+	long n = number;
+	int count = 0;
+
+	while (n != 0)
+	{
+		n /= 10;
+		++count;
+	}
+
+	return count;
+}
 
 void main(void)
 {
-	int input;
-	printf("Here is a sample output with a prompt: ");
+	int const input1 = get_number();
+	int const input2 = get_number();
+	int const sum = input1 + input2;
+	int const product = input1 * input2;
 
-	scanf("%d", &input);
-
-	printf("Here is the inputted number: %d \n", input);
-
-	// Below prevents console window from closing after program completion
-	while (getchar() != '\n');
-	printf("press enter to continue...\n");
-	getchar();
+	printf("The sum of %d and %d is %d\n\n", input1, input2, sum);
+	printf("The product of %d and %d is %d\n\n", input1, input2, product);
+	printf("%d has %d digits.\n\n", input1 * input2, num_digits(product));
 }
